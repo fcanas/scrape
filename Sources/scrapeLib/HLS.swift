fcanas/scrape

commit a46a46e8b72e2a2f378d11b2116754eada4c0311

Use HLSCore for HLS playlist parsing
Lets scrape correctly pull media in renditions, e.g. alternate audio and
subtitles.

Sources/scrapeLib/HLS.swift
@@ -7,20 +7,25 @@
 //
 
 import Foundation
+import Parsing
 
 // Lines that are neither comments nor tags are URLs to resources - and don't begin with #
 let resourceExpression = try! NSRegularExpression(pattern: "^(?!#).+", options: [.anchorsMatchLines])
 
 /// Given an HLS manifest string, and the manifest's URL, generates an array of
 /// resource URLs specified in the manifest.
-func resourceURLs(_ manifestString: NSString, manifestURL: URL) -> [URL] {
-    let checkingResults :[NSTextCheckingResult] = resourceExpression.matches(in: manifestString as String, options: [], range: manifestString.fullRange)
-    
-    return checkingResults.map({ (r :NSTextCheckingResult) -> URL in
-        let resourceString = manifestString.substring(with: r.range)
-        let resourceURL = URL(string: resourceString, relativeTo: manifestURL)!.absoluteURL
-        return resourceURL
-    })
+public func resourceURLs(_ manifestString: NSString, manifestURL: URL) -> [URL] {
+    let s = manifestString as String
+
+    let master = parseMasterPlaylist(string: s, atURL: manifestURL)
+    let renditionURLs = master?.renditions.compactMap({ (rendition) -> URL? in
+        rendition.uri
+    }) ?? []
+    let streamURLs = master?.streams.map({ (stream) -> URL in
+        stream.uri
+    }) ?? []
+    // De-duplicate URLs
+    return Array(Set(streamURLs + renditionURLs))
 }
 
 let fileManager = FileManager.default

Tests/scrapeTests/scrapeTests.swift
@@ -2,8 +2,39 @@
 
 import XCTest
 import Foundation
-@testable import scrape
+import scrapeLib
 
+class URLExtractionTests: XCTestCase {
+
+    func testURLExtraction() {
+        let optURL = URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8")
+        guard let url = optURL, let manifestString = try? String(contentsOf: url) else {
+            XCTFail("URL test stream at \(String(describing: optURL)) failed.")
+            return
+        }
+
+        let urls = resourceURLs(manifestString as NSString, manifestURL: url).map({$0.absoluteURL})
+
+        let expectedURLs = [
+            "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/v5/prog_index.m3u8",
+            "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/v9/prog_index.m3u8",
+            "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/v8/prog_index.m3u8",
+            "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/v7/prog_index.m3u8",
+            "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/v6/prog_index.m3u8",
+            "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/v4/prog_index.m3u8",
+            "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/v3/prog_index.m3u8",
+            "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/v2/prog_index.m3u8",
+
+            "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/a1/prog_index.m3u8",
+            "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/a2/prog_index.m3u8",
+            "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/a3/prog_index.m3u8",
+            "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/s1/en/prog_index.m3u8"]
+        XCTAssertEqual(Set(urls), Set(expectedURLs.map({URL(string:$0)!})))
+    }
+
+}
+
+/*
 class StringExtensionTests: XCTestCase {
     
     func testFullRange() {
@@ -62,5 +93,5 @@ class URLExtensionTests: XCTestCase {
     }
     
 }
-
+*/
 

Package.resolved
@@ -1,0 +1,25 @@
+{
+  "object": {
+    "pins": [
+      {
+        "package": "FFCParserCombinator",
+        "repositoryURL": "https://github.com/fcanas/FFCParserCombinator.git",
+        "state": {
+          "branch": null,
+          "revision": "b9510a294c3cdd4003a847ab3b7fe7b723e3878c",
+          "version": "0.0.5"
+        }
+      },
+      {
+        "package": "HLSCore",
+        "repositoryURL": "https://github.com/fcanas/HLSCore.git",
+        "state": {
+          "branch": null,
+          "revision": "16223d881a2d44c2a13aef05d639783b9cfbdcd5",
+          "version": "0.0.2"
+        }
+      }
+    ]
+  },
+  "version": 1
+}

Package.swift
@@ -1,5 +1,25 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-    name: "scrape"
+    name: "scrape",
+    products: [
+        .library(
+            name:"scrapeLib",
+            type:.dynamic,
+            targets: ["scrapeLib"]
+        ),
+        .executable(
+            name: "scrape",
+            targets: ["scrape"]
+        )
+    ],
+    dependencies:[
+        .package(url: "https://github.com/fcanas/HLSCore.git", .upToNextMinor(from: "0.0.2"))
+    ],
+    targets: [
+        .target(name: "scrape", dependencies: ["scrapeLib"]),
+        .target(name: "scrapeLib", dependencies: ["HLSCore"]),
+        .testTarget(name: "scrapeTests", dependencies: ["scrapeLib"])
+    ]
 )

Sources/scrape/main.swift
@@ -7,12 +7,13 @@
 //
 
 import Foundation
+import scrapeLib
 
 /// Argument Parsing
 
 let processInfo = ProcessInfo()
 
-var args = Array(processInfo.arguments[1..<processInfo.arguments.count])
+var args: [String] = Array(processInfo.arguments[1..<processInfo.arguments.count])
 
 func printUsage() {
     print("usage: scrape [-p] input_url output_url")
@@ -48,14 +49,6 @@ enum Option: String {
 let group = DispatchGroup()
 
 let downloader = Downloader(destination: destinationURL, group: group)
-
-fileprivate var verbose = false
-
-func diagnostic(_ string: String) {
-    if verbose {
-        print(string)
-    }
-}
 
 while let arg = args.popLast() {
     guard let option = Option(rawValue: arg) else {

Sources/scrapeLib/Diagnostic.swift
@@ -1,0 +1,16 @@
+//
+//  Diagnostic.swift
+//  scrapeLib
+//
+//  Created by Fabian Canas on 4/14/18.
+//
+
+import Foundation
+
+public var verbose: Bool = false
+
+func diagnostic(_ string: String) {
+    if verbose {
+        print(string)
+    }
+}

Sources/scrapeLib/Download.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class Downloader : NSObject, URLSessionDelegate, URLSessionDownloadDelegate {
+public class Downloader : NSObject, URLSessionDelegate, URLSessionDownloadDelegate {
 
     private let group: DispatchGroup
 
@@ -18,29 +18,29 @@ class Downloader : NSObject, URLSessionDelegate, URLSessionDownloadDelegate {
     
     let destination :URL
 
-    var urlFilter :(URL) -> Bool = { _ in true }
+    public var urlFilter :(URL) -> Bool = { _ in true }
     
-    init(destination: URL, group: DispatchGroup) {
+    public init(destination: URL, group: DispatchGroup) {
         self.destination = destination
         self.group = group
     }
     
     /// Download all resources in a manifest specified at the URL, recursively if a
     /// resource is itself an m3u8 manifest.
-    func downloadHLSResource(_ downloadURL: URL) {
+    public func downloadHLSResource(_ downloadURL: URL) {
         let task = session.downloadTask(with: downloadURL)
         group.enter()
         task.resume()
     }
     
-    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+    public func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
         guard let resourceURL = downloadTask.currentRequest?.url else {
             return
         }
         ingestHLSResource(resourceURL, temporaryFileURL: location, downloader: self.downloadHLSResource, destinationURL: self.destination, urlFilter: urlFilter)
     }
     
-    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+    public func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
         diagnostic("\(error != nil ? "failed : \(error!)" : "success") : \(task.originalRequest?.url?.absoluteString ?? "no URL")")
         group.leave()
     }

commit 9d496147fad40f2895046ea4988f346365720700

Extract URLs from media playlists
Fixes regression introduced in a46a46e8b72e2a2f378d11b2116754eada4c0311

Sources/scrapeLib/HLS.swift
@@ -17,15 +17,24 @@ let resourceExpression = try! NSRegularExpression(pattern: "^(?!#).+", options: 
 public func resourceURLs(_ manifestString: NSString, manifestURL: URL) -> [URL] {
     let s = manifestString as String
 
-    let master = parseMasterPlaylist(string: s, atURL: manifestURL)
-    let renditionURLs = master?.renditions.compactMap({ (rendition) -> URL? in
-        rendition.uri
-    }) ?? []
-    let streamURLs = master?.streams.map({ (stream) -> URL in
-        stream.uri
-    }) ?? []
+    var urls: Set<URL> = Set()
+    if let master = parseMasterPlaylist(string: s, atURL: manifestURL) {
+        _ = master.renditions.compactMap({ rendition in
+            rendition.uri.map({urls.insert($0)})
+        })
+        _ = master.streams.map({ stream in
+            urls.insert(stream.uri)
+        })
+    }
+
+    if let media = parseMediaPlaylist(string: s, atURL: manifestURL) {
+        _ = media.segments.map { segment in
+            urls.insert(segment.resource.uri)
+        }
+    }
+
     // De-duplicate URLs
-    return Array(Set(streamURLs + renditionURLs))
+    return Array(urls)
 }
 
 let fileManager = FileManager.default

Tests/scrapeTests/scrapeTests.swift
@@ -6,7 +6,7 @@ import scrapeLib
 
 class URLExtractionTests: XCTestCase {
 
-    func testURLExtraction() {
+    func testMasterPlaylistURLExtraction() {
         let optURL = URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8")
         guard let url = optURL, let manifestString = try? String(contentsOf: url) else {
             XCTFail("URL test stream at \(String(describing: optURL)) failed.")
@@ -32,66 +32,34 @@ class URLExtractionTests: XCTestCase {
         XCTAssertEqual(Set(urls), Set(expectedURLs.map({URL(string:$0)!})))
     }
 
-}
+    func testMediaPlaylistURLExtraction() {
+        let optURL = URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/v2/prog_index.m3u8")
+        guard let url = optURL, let manifestString = try? String(contentsOf: url) else {
+            XCTFail("URL test stream at \(String(describing: optURL)) failed.")
+            return
+        }
 
-/*
-class StringExtensionTests: XCTestCase {
-    
-    func testFullRange() {
-        let emptyString :NSString = ""
-        let singleCharacter :NSString = "A"
-        let multipleLines :NSString = "Something on\nmultiple lines\nis good to test."
-        let basic :NSString = "Some basic string"
-        
-        let allStrings = [emptyString, singleCharacter, multipleLines, basic]
-        
-        for s in allStrings {
-            XCTAssertEqual(s as String, s.substring(with: s.fullRange))
+        let urls = resourceURLs(manifestString as NSString, manifestURL: url).map({$0.absoluteURL})
+
+        let expectedURLs = [
+            "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/v2/main.mp4"]
+        XCTAssertEqual(Set(urls), Set(expectedURLs.map({URL(string:$0)!})))
+    }
+
+    func testMediaPlaylistSubtitleURLExtraction() {
+        let optURL = URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/s1/en/prog_index.m3u8")
+        guard let url = optURL, let manifestString = try? String(contentsOf: url) else {
+            XCTFail("URL test stream at \(String(describing: optURL)) failed.")
+            return
+        }
+
+        let urls = Set(resourceURLs(manifestString as NSString, manifestURL: url).map({$0.absoluteURL}))
+
+        XCTAssertEqual(urls.count, 100)
+        for i in 0...99 {
+            XCTAssertTrue(urls.contains(URL(string:"https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/s1/en/fileSequence\(i).webvtt")!))
         }
     }
 
-    func testdeepestDirectoryPath() {
-        let f = "/path/with/file"
-        XCTAssertEqual(f.deepestDirectoryPath(), "/path/with/")
-        
-        let d = "/path/without/file/"
-        XCTAssertEqual(d.deepestDirectoryPath(), "/path/without/file/")
-    }
-    
 }
-
-class URLExtensionTests: XCTestCase {
-    
-    func testDetectSegment() {
-        let segment = URL(string: "http://example.com/hls/stream/segment01.ts")
-        let query = URL(string: "http://example.com/hls/stream/segment01.ts?q=thing")
-        let fragment = URL(string: "http://example.com/hls/stream/segment01.ts#fragment")
-        
-        XCTAssertEqual(segment?.type, .Media)
-        XCTAssertEqual(query?.type, .Media)
-        XCTAssertEqual(fragment?.type, .Media)
-    }
-    
-    func testDetectPlaylist() {
-        let segment = URL(string: "http://example.com/hls/stream/segment01.m3u8")
-        let query = URL(string: "http://example.com/hls/stream/segment01.m3u8?q=thing")
-        let fragment = URL(string: "http://example.com/hls/stream/segment01.m3u8#fragment")
-        
-        XCTAssertEqual(segment?.type, .Playlist)
-        XCTAssertEqual(query?.type, .Playlist)
-        XCTAssertEqual(fragment?.type, .Playlist)
-    }
-    
-    func testIgnoreNonHLSTypes() {
-        let nonSegment = URL(string: "http://example.com/hls/stream/segment01.mp3")
-        let query = URL(string: "http://example.com/hls/stream/segment01.aac?q=thing")
-        let fragment = URL(string: "http://example.com/hls/stream/segment01.tts#fragment")
-        
-        XCTAssertEqual(nonSegment?.type, .Media)
-        XCTAssertEqual(query?.type, .Media)
-        XCTAssertEqual(fragment?.type, .Media)
-    }
-    
-}
-*/
 