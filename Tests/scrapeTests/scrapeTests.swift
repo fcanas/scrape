bd1b4c6192352f76f121fc01f28040f019ea830b
Fabian Canas <fcanas@gmail.com>
2018-05-11 21:01:34 -0400

    Expand tests to cover downloading of webvtt caption tracks

--- Tests/scrapeTests/scrapeTests.swift
+++ Tests/scrapeTests/scrapeTests.swift
@@ -32,6 +32,49 @@ class URLExtractionTests: XCTestCase {
         XCTAssertEqual(Set(urls), Set(expectedURLs.map({URL(string:$0)!})))
     }
 
+    func testMasterPlaylistURLExtractionWithExternalCaptions() {
+        let optURL = URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8")
+
+        guard let url = optURL, let manifestString = try? String(contentsOf: url) else {
+            XCTFail("URL test stream at \(String(describing: optURL)) failed.")
+            return
+        }
+
+        let urls = resourceURLs(manifestString as NSString, manifestURL: url).map({$0.absoluteURL})
+
+        let expectedURLs = [
+            "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/v3/prog_index.m3u8",
+            "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/s1/en/prog_index.m3u8",
+            "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/v5/prog_index.m3u8",
+            "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/v6/prog_index.m3u8",
+            "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/v9/prog_index.m3u8",
+            "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/a2/prog_index.m3u8",
+            "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/v7/prog_index.m3u8",
+            "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/v2/prog_index.m3u8",
+            "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/v8/prog_index.m3u8",
+            "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/a1/prog_index.m3u8",
+            "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/a3/prog_index.m3u8",
+            "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/v4/prog_index.m3u8"
+        ]
+
+        XCTAssertEqual(Set(urls), Set(expectedURLs.map({URL(string:$0)!})))
+    }
+
+    func testSubtitlePlaylistExtraction() {
+        let optURL = URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/s1/en/prog_index.m3u8")
+
+        guard let url = optURL, let manifestString = try? String(contentsOf: url) else {
+            XCTFail("URL test stream at \(String(describing: optURL)) failed.")
+            return
+        }
+
+        let urls = resourceURLs(manifestString as NSString, manifestURL: url).map({$0.absoluteURL})
+
+        let expectedURLs = (0...99).map { URL(string:"https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/s1/en/fileSequence\($0).webvtt")! }
+
+        XCTAssertEqual(Set(urls).symmetricDifference(Set(expectedURLs)), Set())
+    }
+
     func testMediaPlaylistURLExtraction() {
         let optURL = URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/v2/prog_index.m3u8")
         guard let url = optURL, let manifestString = try? String(contentsOf: url) else {
@@ -62,4 +105,3 @@ class URLExtractionTests: XCTestCase {
     }
 
 }
-
